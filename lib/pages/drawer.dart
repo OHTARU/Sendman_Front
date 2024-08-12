5adef45003cf7284901346ac1d3a2850d6f7ecda
성근 <sk020603@naver.com>
2024-08-12 16:04:55 +0900

    drawer 로그인 !=null부분 처리

--- lib/pages/drawer.dart
+++ lib/pages/drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/colors/colors.dart';
 import 'package:flutter_application_1/pages/page_tts.dart';
 import 'package:flutter_application_1/pages/photo_to_text.dart';
 import 'package:flutter_application_1/pages/stt_list.dart';
@@ -207,7 +208,203 @@ class BaseDrawer extends StatelessWidget {
         ),
       );
     } else {
-      return const Drawer();
+      return Drawer(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  DrawerHeader(
+                    padding: EdgeInsets.zero,
+                    margin: const EdgeInsets.only(bottom: 100),
+                    child: Column(
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            CloseButton(),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              '로그인',
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w700,
+                                  color: mainBlueColor),
+                            )),
+                        const SizedBox(
+                          height: 15,
+                        )
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 5),
+                    title: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(Icons.mic),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "음성 - 글자",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => const SendManDemo()));
+                    },
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 5),
+                    title: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(Icons.mic),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "글자 - 음성",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TextToSpeech()));
+                    },
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 5),
+                    title: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(Icons.image),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "사진 - 글자",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const PhotoToText()));
+                    },
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 5),
+                    title: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(Icons.attach_file),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "음성 텍스트 보기",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TtsList()));
+                    },
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 5),
+                    title: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(Icons.attach_file),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          "사진 텍스트 보기",
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SttList()));
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 18),
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: const Color.fromARGB(255, 16, 38, 104),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      '로그아웃',
+                      style: (TextStyle(
+                          fontWeight: FontWeight.w800, fontSize: 20)),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
     }
   }
 }
